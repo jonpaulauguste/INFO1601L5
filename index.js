f89f5653326809d079465ab3eb8e7f202fdab837
jonpaulauguste <144620333+jonpaulauguste@users.noreply.github.com>
2025-02-18 09:14:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*
+
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
 
 for(let i=0; i< arr.length; i++){
@@ -167,9 +169,15 @@ function happyPrint(string){
  add(5, 10, happyPrint);
  add(11, 12, sadPrint);
 
+*/
+
+
  //prints current date
 function printDate(){
   console.log(new Date().toLocaleTimeString());
+  if (stop){
+    clearInterval(interval);
+  }
 }
      
 setInterval(printDate, 1000);
